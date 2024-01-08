b4a306d3207dfad659371b8367807798c14c7f45
Sergej Jaudszims <redtape9688@gmail.com>
2024-01-08 13:46:36 +0100

    create backend module and update .gitignore

---
+++ backend/src/test/java/io/github/redtape9/nextupmanager/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.redtape9.nextupmanager.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
