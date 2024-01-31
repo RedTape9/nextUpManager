1bb5471150e596ff38ec818338a02437f21b0903
Sergej Jaudszims <redtape9688@gmail.com>
2024-01-31 09:45:06 +0100

    delete old test file

--- backend/src/test/java/io/github/redtape9/nextupmanager/backend/NextUpCustomerControllerTest.java
+++
@@ -1,8 +0,0 @@
-package io.github.redtape9.nextupmanager.backend;
-
-import org.springframework.test.web.servlet.MockMvc;
-
-public class NextUpCustomerControllerTest {
-
-    private MockMvc mockMvc;
-}
