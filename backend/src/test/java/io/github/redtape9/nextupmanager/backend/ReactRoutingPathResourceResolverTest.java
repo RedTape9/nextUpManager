1d3f3b1228afce92058b1989a10e4498f3544ed2
Sergej Jaudszims <redtape9688@gmail.com>
2024-02-02 15:48:09 +0100

    fix whitelabel error page

---
+++ backend/src/test/java/io/github/redtape9/nextupmanager/backend/ReactRoutingPathResourceResolverTest.java
@@ -0,0 +1,52 @@
+package io.github.redtape9.nextupmanager.backend;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+class ReactRoutingPathResourceResolverTest {
+
+    @Test
+    void expectRelativeResource_ifItExists() throws IOException {
+
+        // GIVEN
+        var resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
+        var location = mock(Resource.class);
+        var relativeLocation = mock(Resource.class);
+        var resourcePath = "index.html";
+        when(location.createRelative(resourcePath)).thenReturn(relativeLocation);
+        when(relativeLocation.exists()).thenReturn(true);
+        when(relativeLocation.isReadable()).thenReturn(true);
+
+        // WHEN
+        var actual = resolver.resolveResource(null, resourcePath, null, null);
+
+        // THEN
+        Assertions.assertEquals(relativeLocation, actual);
+    }
+
+    @Test
+    void expectIndexHtml_ifRequestedResourceDoesNotExist() throws IOException {
+
+        // GIVEN
+        var resolver = new ReactRoutingForwarding.ReactRoutingPathResourceResolver();
+        var location = mock(Resource.class);
+        var relativeLocation = mock(Resource.class);
+        var resourcePath = "index.html";
+        when(location.createRelative(resourcePath)).thenReturn(relativeLocation);
+        when(relativeLocation.exists()).thenReturn(false);
+
+        // WHEN
+        var actual = resolver.resolveResource(null, resourcePath, null, null);
+
+        // THEN
+        ClassPathResource expected = new ClassPathResource("static/index.html");
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
