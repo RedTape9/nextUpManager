a2b9ba7c243c1e7ee07f34923f5b55f7295ef65e
Sergej Jaudszims <redtape9688@gmail.com>
2024-02-21 18:32:10 +0100

    add getAllEmployees_ShouldReturnAllEmployees Test

--- backend/src/test/java/io/github/redtape9/nextupmanager/backend/EmployeeServiceTests.java
+++ backend/src/test/java/io/github/redtape9/nextupmanager/backend/EmployeeServiceTests.java
@@ -1,6 +1,7 @@
 package io.github.redtape9.nextupmanager.backend;
 
 import io.github.redtape9.nextupmanager.backend.dto.EmployeeGetForDetailsDTO;
+import io.github.redtape9.nextupmanager.backend.dto.EmployeeGetForOptionDTO;
 import io.github.redtape9.nextupmanager.backend.entity.Employee;
 import io.github.redtape9.nextupmanager.backend.repo.EmployeeRepository;
 import io.github.redtape9.nextupmanager.backend.service.EmployeeService;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,18 +27,26 @@ public class EmployeeServiceTests {
     private EmployeeService employeeService;
 
     Employee employee;
+    Employee employee2;
 
     @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
 
-        employee = new Employee();
-        employee.setId("1");
-        employee.setName("Franz");
-        employee.setSurname("Broetchen");
-        employee.setDepartmentId("Kueche");
-        employee.setRoom("K01");
+        employee = createEmployee("1", "Franzi", "Broetchen", "Kueche", "K01");
+        employee2 = createEmployee("2", "Hans", "Wurst", "Kueche", "K01");
     }
+
+    private Employee createEmployee(String id, String name, String surname, String departmentId, String room) {
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setName(name);
+        employee.setSurname(surname);
+        employee.setDepartmentId(departmentId);
+        employee.setRoom(room);
+        return employee;
+    }
+
 
     @Test
     void getEmployeeById_ShouldReturnEmployee_WhenExists() {
@@ -54,7 +66,33 @@ public class EmployeeServiceTests {
         assertEquals(employee.getRoom(), result.getRoom());
     }
 
+    @Test
+    void getAllEmployees_ShouldReturnAllEmployees() {
+        // Given
+        List<Employee> employees = new ArrayList<>();
+        employees.add(employee);
+        employees.add(employee2);
 
+        when(employeeRepository.findAll()).thenReturn(employees);
+
+        // When
+        List<EmployeeGetForOptionDTO> result = employeeService.getAllEmployees();
+
+        // Then
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals(employee.getId(), result.get(0).getId());
+        assertEquals(employee.getName(), result.get(0).getName());
+        assertEquals(employee.getSurname(), result.get(0).getSurname());
+        assertEquals(employee.getRoom(), result.get(0).getRoom());
+        assertEquals(employee2.getId(), result.get(1).getId());
+        assertEquals(employee2.getName(), result.get(1).getName());
+        assertEquals(employee2.getSurname(), result.get(1).getSurname());
+        assertEquals(employee2.getRoom(), result.get(1).getRoom());
+
+
+    }
 }
+
 
 
