854e6c255073e59e5aeea70a2ba4fbf4e4087cf8
Sergej Jaudszims <redtape9688@gmail.com>
2024-02-24 10:22:52 +0100

    add space between in footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,7 +7,7 @@ function Footer() {
                 <Row>
                     <Col>
                         <p>2024 <a href="https://github.com/RedTape9/nextUpManager" target="_blank" rel="noopener noreferrer" className="text-primary">RedTape9</a>
-                            - Open Source Project</p>
+                            &nbsp;- Open Source Project</p>
                         <p className="fs-6">favicon by <a href="https://favicon.io/emoji-favicons/man-surfing" target="_blank" rel="noopener noreferrer" className="text-primary">favicon.io</a></p>
                     </Col>
                 </Row>
